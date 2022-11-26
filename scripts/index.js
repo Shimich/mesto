e188ca4fc98e3d61cb21c6014c0dea29c5ff5df5
Ilya Shimich <xobbit170@mail.ru>
2022-11-26 06:41:14 +0300

    asynk defer

--- scripts/index.js
+++ scripts/index.js
@@ -31,42 +31,6 @@ const popupInfoSaveButton = popupInfo.querySelector('.popup__save');
 
 const popupCloseButtons = document.querySelectorAll('.popup__close');
 
-const initialCards = [
-    {
-        place: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        place: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        place: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        place: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        place: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        place: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
-
-const popupSelectors = {
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__save',
-    inactiveButtonClass: 'popup__save_inactive',
-    inputErrorClass: 'popup__input_type_error',
-    errorClass: 'popup__input-error_active'
-};
-
 function openPopup(popup) {
     popup.classList.add('popup_is-opened');
     document.addEventListener('keydown',closePopupByEsc);
